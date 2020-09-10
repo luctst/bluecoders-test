5cbe99cca0687cb781002ce0f30f3d4a664bfdf6
luctst <lucas.tostee@gmail.com>
2020-09-10 17:38:58 +0200

    Add logic for header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,82 @@
 <template>
   <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav
+      class="fixed-top navbar navbar-expand-lg navbar navbar-light"
+      style="background-color: #e3f2fd;"
+    >
       <router-link to="/">Bluecoders</router-link>
+      <div v-if="accountData.id" class="collapse navbar-collapse" style="flex-basis:auto">
+        <ul class="navbar-nav ml-auto align-items-end">
+          <li class="nav-item">
+            <router-link
+              :to="{ path: `/user/${accountData.id}`}">
+              {{accountData.mail}}
+            </router-link>
+          </li>
+          <li class="nav-item ml-4">
+            <p class="text-danger mb-0" @click="disconnect">Déconnexion</p>
+          </li>
+        </ul>
+      </div>
     </nav>
   </header>
 </template>
 
 <script>
-export default {
+import { mapState, mapMutations } from 'vuex';
 
+export default {
+  computed: mapState([
+    'accountData',
+    'jwt',
+  ]),
+  methods: {
+    ...mapMutations([
+      'resetStore',
+    ]),
+    disconnect() {
+      return fetch('http://localhost:3000/api/logout', {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${this.jwt}`,
+        },
+      })
+        .then(() => {
+          this.resetStore();
+          this.$router.push('/login');
+          return null;
+        });
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+header {
+  .collapse:not(.show) {
+    display: flex;
+  }
+
+  nav {
+    height: 8vh;
+    max-height: 8vh;
+
+    div {
+      flex-basis: auto;
+
+      ul {
+        li:last-child:hover {
+          cursor: pointer;
+        }
+
+        li {
+          a,
+          p {
+            font-size: 0.8em;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
