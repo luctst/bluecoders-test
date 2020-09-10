17cd89885e93661774fda2d695af090e1ad8b7d4
luctst <lucas.tostee@gmail.com>
2020-09-10 17:39:44 +0200

    Remove useless import

--- api/routes/logout.js
+++ api/routes/logout.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const { ObjectID, ObjectId } = require("mongodb");
+const { ObjectId } = require("mongodb");
 
 const isConnected = require("../middlewares/isConnected");
 const response = require("../utils/response");
