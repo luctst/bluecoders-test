19d3665b40dd52e2f5911ac8f4b7ec9e3799879e
luctst <lucas.tostee@gmail.com>
2020-09-10 12:33:33 +0200

    Add api db controlllers

---
+++ api/db/register.js
@@ -0,0 +1,36 @@
+const {hash} = require("bcrypt");
+
+const sessionLogic = require("../utils/sessionLogic");
+
+/**
+ * Create a new user if not already register.
+ * @param {Object} data - The date send by the request body
+ * @param {Object} mongo - The mongo client instance use for manipulate bdd
+ */
+module.exports = async function registerController (data, mongo) {
+  const usersCol = mongo.db().collection("users");
+
+  if ((await usersCol.findOne({mail: data.mail})) === null) {
+    const passwordHashed = await hash(data.password, 10);
+    const newUser = await usersCol.insertOne({
+      mail: data.mail,
+      password: passwordHashed,
+      tasks: {
+        todo: [],
+        doing: [],
+        done: []
+      }
+    });
+
+    const sessionData = await sessionLogic(mongo, newUser);
+    return {
+      code: 200,
+      header: sessionData.header,
+      forClient: sessionData.forClient
+    }
+  }
+
+  return {
+    code: 409
+  }
+}
