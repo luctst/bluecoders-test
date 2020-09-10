9e8827ad7e2b1592545cd1a6d3ebaaf66a57f6ca
luctst <lucas.tostee@gmail.com>
2020-09-10 17:41:50 +0200

    Create mutations store

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,22 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     jwt: '',
+    accountData: {},
   },
   mutations: {
+    connected(state, data) {
+      state.jwt = data.jwt;
+      state.accountData = { ...data };
+
+      delete state.accountData.jwt;
+    },
+    resetStore(state) {
+      state.jwt = '';
+      state.accountData = {};
+    },
+    updateJwt(state, newJWt) {
+      state.jwt = newJWt;
+    },
   },
   actions: {
   },
