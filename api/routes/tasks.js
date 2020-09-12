9500ee65b6c9ec38546823eff96ddb3a6b214ec7
luctst <lucas.tostee@gmail.com>
2020-09-12 16:21:30 +0200

    Add logic for update task in bdd

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -82,14 +82,73 @@ router.post("/", isConnected, async function (req, res) {
   });
 });
 
-router.put('/:id', function (req, res) {
+router.put('/:id', isConnected, async function (req, res) {
   const bodyArray = Object.keys(req.body);
 
-  if (bodyArray.length > 6) {
+  if (bodyArray.length > 4) {
     return response(res, 400);
   }
+
+  const checkProps = bodyArray.map(function (l) {
+    if (['title', 'description', 'tags'].includes(l)) {
+      return true;
+    }
+
+    return false;
+  });
+
+  if (checkProps.includes(false)) {
+    return response(res, 422);
+  }
+
+  const tasksCol = res.locals.mongo.db().collection('tasks');
+  const l = await tasksCol.findOneAndUpdate(
+    {_id: new ObjectId(req.params.id)},
+    {
+      $set: { ...req.body }
+    }
+  );
+
+  if (!l.ok) {
+    return response(res, 502);
+  }
+
+  if (res.locals.data.forClient) {
+    return response(res, 200, {
+      header: res.locals.data.header,
+      newResponse: { ...res.locals.data.forClient },
+    })
+  }
+
+  return response(res, 204);
 });
 
-router.delete("/:id", function (req, res) {});
+router.delete("/:id", isConnected, async function (req, res) {
+  const tasksCol = res.locals.mongo.db().collection('tasks');
+  const deletePassed = await tasksCol.findOneAndDelete(
+    {_id: new ObjectId(req.params.id)}
+  );
+
+  if (!deletePassed.ok) {
+    return response(res, 502);
+  }
+
+  const newTasksList = await tasksCol.find().toArray();
+
+  if (res.locals.data.forClient) {
+    res.locals.data.forClient.tasks = [...newTasksList];
+
+    return response(res, 200, {
+      header: res.locals.data.header,
+      newResponse: {...res.locals.data.forClient}
+    })
+  }
+
+  return response(res, 200, {
+    newResponse: {
+      tasks: [...newTasksList],
+    },
+  });
+});
 
 module.exports = router;
