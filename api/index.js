1e27089749ffc98cf4e3444e275ccd3a1f33820c
luctst <lucas.tostee@gmail.com>
2020-09-12 11:13:05 +0200

    Add more middlewares

--- api/index.js
+++ api/index.js
@@ -1,21 +1,26 @@
 require("dotenv").config();
-const app = require("express")();
+const express = require("express");
+const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const compression = require("compression");
 
 const connectBdd = require("./middlewares/connectBdd");
+
 const registerRouter = require("./routes/register");
 const loginRouter = require("./routes/login");
 const tasksRouter = require("./routes/tasks");
 const logoutRouter = require("./routes/logout");
 
-app.use(connectBdd());
 app.use(compression());
 app.use(cors({
   origin: "http://localhost:8080"
 }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(connectBdd());
 
 app.use("/api/register", registerRouter);
 app.use("/api/login", loginRouter);
