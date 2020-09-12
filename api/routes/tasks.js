ce74c9bc07dabf29ccfc18b95d1917a7bd786f01
luctst <lucas.tostee@gmail.com>
2020-09-12 11:15:03 +0200

    Create logic for /tasks route

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -1,15 +1,12 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
+const {ObjectId} = require("mongodb");
 
-const isConnected = require("../middlewares/isConnected");
 const response = require("../utils/response");
+const isConnected = require("../middlewares/isConnected");
 
 const router = express.Router();
 
-router.use(cookieParser());
-router.use(isConnected());
-
-router.get("/", async function (req, res) {
+router.get("/", isConnected,  async function (req, res) {
   const forClient = {};
   const tasksCol = res.locals.mongo.db().collection("tasks");
 
@@ -27,7 +24,70 @@ router.get("/", async function (req, res) {
   });
 });
 
-router.post("/", function (req, res) {
+router.get('/:id', isConnected, async function (req, res) {
+  const forClient = {};
+  const tasksCol = res.locals.mongo.db().collection('tasks');
+
+  const task = await tasksCol.findOne(
+    { _id: new ObjectId(req.params.id)},
+    {
+      projection: { _id: 0 }
+    }
+  );
+
+  if (res.locals.data.forClient) {
+    forClient.token = res.locals.data.forClient.token;
+  }
+
+  forClient.task = task;
+  return response(res, 200, {
+    header: res.locals.data.header,
+    newResponse: {...forClient}
+  })
+});
+
+router.post("/", isConnected, async function (req, res) {
+  const bodyArray = Object.keys(req.body);
+  const forClient = {};
+
+  if (bodyArray.length > 2) {
+    return response(res, 400);
+  }
+
+  if (!bodyArray.includes('title') || !bodyArray.includes('author')) {
+    return response(res, 422);
+  }
+
+  const taskToInsert = {
+    ...req.body,
+    description: '',
+    created: new Date(),
+    status: "todo",
+    tags: [],
+  };
+  const tasksCol = res.locals.mongo.db().collection('tasks');
+  await tasksCol.insertOne({ ...taskToInsert });
+
+  const tasksList = await tasksCol.find().toArray();
+
+  if (res.locals.data.forClient) {
+    forClient.token = res.locals.data.forClient.token;
+  }
+
+  forClient.tasks = [...tasksList];
+
+  return response(res, 200, {
+    header: res.locals.data.header,
+    newResponse: { ...forClient }
+  });
+});
+
+router.put('/:id', function (req, res) {
+  const bodyArray = Object.keys(req.body);
+
+  if (bodyArray.length > 6) {
+    return response(res, 400);
+  }
 });
 
 router.delete("/:id", function (req, res) {});
