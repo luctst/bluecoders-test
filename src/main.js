1864bd73215688438e36501b5a7aff935495c82f
luctst <lucas.tostee@gmail.com>
2020-09-14 13:45:17 +0200

    Init vue-socket-io

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import Vue from 'vue';
+import VueSocketIo from 'vue-socket.io';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.use(new VueSocketIo({
+  debug: true,
+  connection: 'http://localhost:3000',
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_',
+  },
+}));
 
 new Vue({
   router,
